35f1287b6bf6a757377d3f2bca6ae2f35d228719
takinbiyi127001 <51786708+takinbiyi127001@users.noreply.github.com>
2022-03-02 11:08:50 +1000

    Added aggregate and ordering

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False) # harry-potter-1
+    slug = models.SlugField(default="", null=False, db_index=True) # harry-potter-1
 
     # Override the builtin save method to add slug from title
     def save(self, *args, **kwargs):
@@ -20,7 +20,7 @@ class Book(models.Model):
 
     # Overrite builtin method get_absolute_url
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])
+        return reverse("book-detail", args=[self.slug])
     
     def __str__(self):
         return f"{self.title} ({self.rating}) {self.author} ({self.is_bestselling}))"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
 
 from .models import Book
 
 def index(request):
-    books = Book.objects.all()
-    context = {"books": books}
+    books = Book.objects.all().order_by("title") # to order by descending order use "-title"
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating")) # rating__avg, rating_min
+    context = {
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating
+    }
     return render(request, "book_outlet/index.html", context=context)
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, pk=id)
+    # book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     context = {
         "title": book.title,
         "author": book.author,
         "rating": book.rating,
         "is_bestseller": book.is_bestselling
     }
-    
+
     return render(request, "book_outlet/book_details.html", context=context)
