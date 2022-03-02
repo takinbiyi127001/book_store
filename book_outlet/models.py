c6774e6f81138ff2f385b90955b452541691c792
takinbiyi127001 <51786708+takinbiyi127001@users.noreply.github.com>
2022-03-03 07:43:30 +1000

    Added Author class for cross model queries

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -6,10 +6,22 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    # author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author,  on_delete=models.CASCADE, null=True, related_name="books") # set a pointer(relationship) to the Author class for author
     is_bestselling = models.BooleanField(default=False)
     # Set blank = True or set editable = False for default behaviour of the slug field.
     # Harry potter 1 => harry-potter-1
